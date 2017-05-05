66d4dffa9e65a94a4a57253052d648ef30a4adf5
ngoctt <ngoctt@aoi-sys.vn>
2017-05-05 14:56:50 +0700

    add form

---
+++ guestbook/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class SignForm(forms.Form):
+    guestbook_name=forms.CharField(label='GuestBook Name')
+    content=forms.CharField(label='Content', widget=forms.Textarea, max_length=10)
---
+++ guestbook/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from guestbook.views import IndexView, SignView
+
+app_name='guestbook'
+urlpatterns = [
+    url(r'^index/$',IndexView.as_view(), name='index'),
+    url(r'^sign/$',SignView.as_view(), name='sign'),
+]
